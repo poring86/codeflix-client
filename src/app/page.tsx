fc2d05724f87ce5b9904f5b52b0397afd836e619
Matheus Lino <matheuslino86@gmail.com.br>
2023-09-20 21:50:19 -0400

    feat: :sparkles: banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,7 +8,17 @@ export default function Home() {
     <div className='relative h-full lg:h-[140vh]'>
       <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'></div>
+          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+            <Image
+              src='/banner2.jpg'
+              alt='The Witcher'
+              fill={true}
+              className='object-cover h-[65vh] object-top lg:h-[95-vh]'
+            />
+          </div>
+
+          
+
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
             The Witcher
           </h1>
@@ -15,7 +26,11 @@ export default function Home() {
             Geralt of Rivia, a solitary monster hunter, struggles to find hist place in a world where people often prove more wicked than beasts.
           </p>
 
-          <button className='w-24'>Play</button>
+          <div className='flex space-x-3'>
+            <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>Play</button>
+            <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>More info</button>
+          </div>
+          
         </div>
         
       </main>
