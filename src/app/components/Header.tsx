7e10dfa66e5b5e61d774911604cb0a6722f7675d
Matheus Lino <matheuslino86@gmail.com.br>
2023-09-24 10:48:24 -0400

    feat: :sparkles: Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header(){
+  return <header className='fixed top-0 z-50 flex w-full items-center justify-between bg-blue-500 px-4 py-4 lg:px-10 lg:py-6'>
+    <img
+          src='https://rb.gy/ulxxee'
+          alt='netflix'
+          width={120}
+          height={120}
+        />
+        <ul className='hidden md:flex md:space-x-4'>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+        </ul>
+  </header>
+}
