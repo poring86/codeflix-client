bdbe8ad6882328a5ece890580a923781085e2029
Matheus Lino <matheuslino86@gmail.com.br>
2023-09-24 20:11:28 -0400

    feat: :sparkles: Header profile

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className='fixed top-0 z-50 flex w-full items-center justify-between bg-black px-4 py-4 lg:px-10 lg:py-6'>
+    <header className='fixed top-0 z-50 flex w-full items-center justify-between bg-black px-4 py-4 transition-all lg:px-10 lg:py-6'>
       <div className="flex items-center space-x-2 md:space-x-8">
         <img
           src='https://rb.gy/ulxxee'
@@ -15,6 +15,15 @@ export default function Header() {
           <li>Latest</li>
         </ul>
       </div>
+      <div className='flex items-center space-x-4'>
+        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+        <img
+          src='https://rb.gy/g1pwyx'
+          alt=''
+          className='cursor-pointer rounded'
+        />
+      </div>
+
     </header>
   )
 }
