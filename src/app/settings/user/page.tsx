ea917f7d9c0933f21cc36056eeeea7dcde13e659
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-07 15:35:51 -0400

    feat: :sparkles: Component error

--- src/app/settings/user/page.tsx
+++ src/app/settings/user/page.tsx
@@ -1,3 +1,4 @@
+import BuggyButton from '@/app/components/BuggyButton'
 import { getUserInfo } from '@/app/lib/settings'
 import React from 'react'
 
@@ -13,6 +14,9 @@ async function User() {
         <p>Name: {name}</p>
         <p>Email: {email}</p>
         <p>Age: {age}</p>
+      </div>
+      <div className='mt-4'>
+        <BuggyButton />
       </div>
     </div>
   )
