90b8d16f58bcb5e82445b69ee7f0fd14dfa9e208
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-09 21:07:41 -0400

    feat: :sparkles: Page login

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div
+      style={{ backgroundImage: 'url(/background2.jpg)' }}
+      className='bg-cover bg-center bg-no-repeat h-screen w-screen text-white opacity-90 scrollbar-hide'>
+      <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+        Pagina de login
+      </div>
+    </div>
+  )
+}
