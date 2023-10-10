c94052547239e9bfc486bfd5a6bf18927a67ca59
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-09 20:17:46 -0400

    feat: :sparkles: Not found component

--- src/app/lib/settings.tsx
+++ src/app/lib/settings.tsx
@@ -1,3 +1,35 @@
+
+
+export interface User {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: Address
+  phone: string
+  website: string
+  company: Company
+}
+
+export interface Company {
+  name: string
+  catchPhrase: string
+  be: string
+}
+
+export interface Address {
+  street: string
+  suite: string
+  city: string
+  zipcode: string
+  geo: Geo
+}
+
+export interface Geo {
+  lat: string
+  lng: string
+}
+
 export const getAppSettings = async (): Promise<{ theme: string, language: string }> => {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -21,4 +53,10 @@ export const getUserInfo = (): Promise<{ name: string; email: string; age: strin
       })
     }, 5000)
   })
+}
+
+export async function getUserById(id: string): Promise<User> {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+  const user = await response.json()
+  return user
 }
