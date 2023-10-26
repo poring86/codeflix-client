8d3e1a6011307a4b3b5eeda695a07e01bedf5458
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-26 18:51:28 -0400

    feat: :sparkles: Login form

--- src/app/auth/register/RegisterForm.tsx
+++ src/app/auth/register/RegisterForm.tsx
@@ -1,14 +1,13 @@
 'use client'
 import React from 'react';
+
 import { AuthForm } from '@/app/components/AuthForm';
 
 
-export default function LoginForm() {
+export default function RegisterForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log('submit from login');
+    console.log('sumbmit from register');
     e.preventDefault();
   };
-  return (
-    <AuthForm formType="login" onSubmit={handleSubmit} />
-  );
+  return <AuthForm formType='register' onSubmit={handleSubmit} />
 }
