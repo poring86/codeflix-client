73fcb5531bf01b40432cfdccc80b43a4486a93aa
Matheus Lino <matheuslino86@gmail.com.br>
2023-12-04 18:53:33 -0400

    feat: :sparkles: Banner video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import { Inter } from 'next/font/google'
 import Header from './components/Header'
 import { MovieRow } from './components/MovieRow'
 import { Banner } from './components/Banner'
+import { getFeaturedMovie } from './service/MovieService'
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
+  const featuredMovie = await getFeaturedMovie('106');
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
       <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
-        <Banner />
+        <Banner movie={featuredMovie} />
 
         <MovieRow sectionTitle='Popular' />
         <MovieRow sectionTitle='Trending' />
